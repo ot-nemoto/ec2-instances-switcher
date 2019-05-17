6b73ad328c3152cf1fbbb73d846c35db6f7a4c87
ot-nemoto <nemoto@opentone.co.jp>
2019-05-17 00:29:53 +0000

    共通モジュールにコードを移動

--- instances/stop.js
+++ instances/stop.js
@@ -1,29 +1,14 @@
 'use strict';
 
-const AWS = require('aws-sdk');
 const PublicHoliday = require('public-holiday');
 const Ec2Controller = require('ec2-controller');
 
 module.exports.handler = function(event, context) {
 
-  var enableStates = ['pending', 'running', 'stopping', 'stopped'];
-
-  var holiday = new PublicHoliday(process.env['public_holiday_api']);
-  if (holiday.isHoliday()) {
+  if ((new PublicHoliday(process.env['public_holiday_api'])).isHoliday()) {
     console.info("Scripts are skipped due to holidays.");
     return;
   }
 
-  var selector = new Ec2Controller(process.env['tag_name']);
-  selector.select(enableStates, function(instanceIds) {
-    if (instanceIds.length > 0) {
-      var ec2 = new AWS.EC2();
-      ec2.stopInstances({InstanceIds: instanceIds}, function(err, data) {
-        if (err) console.log(err, err.stack);
-        else     console.log(data);
-      });
-    } else {
-      console.info("No instance has been specified to stop.");
-    }
-  });
+  (new Ec2Controller(process.env['tag_name'])).stop();
 };
